639b17f269c9e96de6c363fe471ec4917902c584
Kürşat Emre Özkara <kursatemre_70@hotmail.com>
2023-12-15 20:10:38 +0300

    yedek1
    karakter kontrolleri ve düşman kontrolleri belirli kısımları (run, attack, dead) yazıldı.

---
+++ Royal Pledge Proje/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Animator enemyAnimator;
+    
+    public int maxEnemyHealth = 100;
+    private int currentEnemyHealth;
+
+    public float deathAnimationTime = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyAnimator = GetComponent<Animator>();
+        
+        currentEnemyHealth = maxEnemyHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damageValue)
+    {
+        currentEnemyHealth -= damageValue;
+        
+        enemyAnimator.SetTrigger("Hurt");
+        
+        if (currentEnemyHealth <= 0)
+        {
+            CharacterDie();
+        }
+    }
+
+    private void CharacterDie()
+    {
+        print("Enemy Died");
+        enemyAnimator.SetBool("isDeath", true);
+        
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+        
+        Destroy(gameObject, deathAnimationTime);
+    }
+}
--- Royal Pledge Proje/Assets/Scripts/PlayerScript.cs
+++ Royal Pledge Proje/Assets/Scripts/PlayerScript.cs
@@ -1,19 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    // player run speed
     public float speed = 5f;
-    public float jumpForce = 5.3f;
     
-    // private float minX = -7.70f;
-    // private float maxX = 8.70f;
-
+    // player jump force
+    public float jumpForce = 12.2f;
+    
+    // attack variables
+    public int attackDamage = 50;
+    public Transform attackPoint;
+    public float attackRange = 0.9f;
+    public LayerMask enemyLayers;
+    
     private Rigidbody2D rb;
     private Animator playerAnimator;
     
-    private bool isGrounded;
+    // player is ground check variables
+    public Transform groundCheck;
+    public float groundCheckRange = 0.2f;
+    public LayerMask groundLayer;
+    private bool isGround;
+    
     private bool facingRight = true;
 
     private void Start()
@@ -25,6 +37,24 @@ public class PlayerScript : MonoBehaviour
     private void Update()
     {
         MovePlayer();
+        
+        // player jumping
+        // if (Input.GetButtonDown("Jump"))
+        // {
+        //     rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        // }
+        
+        // player jumping
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        // player do attack
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Attack();
+        }
     }
 
     private void Flip()
@@ -41,19 +71,66 @@ public class PlayerScript : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
         
+        // play run animation
         playerAnimator.SetFloat("speed", Mathf.Abs(rb.velocity.x));
-
-        // player jumping
-        if (Input.GetButtonDown("Jump"))
-        {
-            // playerAnimator.SetBool("isJump", true);
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
 
         // player face direction
         if ((horizontalInput > 0 && !facingRight) || (horizontalInput < 0 && facingRight))
         {
             Flip();
         }
+    }
+
+    private void Jump()
+    {
+        // detect grounds
+        isGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRange, groundLayer);
+
+        if (isGround)
+        {
+            print("isGround: " + isGround);
+            
+            playerAnimator.SetBool("isJump", true);
+            
+            // player is jump
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        else
+        {
+            print("isGround: " + isGround);
+            
+            playerAnimator.SetBool("isJump", false);
+        }
+        
+        // foreach (Collider2D ground in isGround)
+        // {
+        //     print("is ground." + ground.name);
+        // }
+    }
+
+    private void Attack()
+    {
+        // play attack animation
+        playerAnimator.SetTrigger("Attack");
+
+        // detect enemies
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        // Damage enemies
+        foreach (Collider2D Enemy in hitEnemies)
+        {
+            print("Hit " + Enemy.name);
+            Enemy.GetComponent<EnemyController>().TakeDamage(attackDamage);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null || groundCheck == null)
+            return;
+        
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRange);
     }
 }
