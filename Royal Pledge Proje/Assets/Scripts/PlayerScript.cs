3d2cfdc35a4acde690832b778a382cd3ff59797a
Kürşat Emre Özkara <kursatemre_70@hotmail.com>
2024-01-03 13:38:20 +0300

    ADD Attack Timer
    Saldırıya zamanlayıcı eklendi. Inspectora düzenlemeler eklendi.

--- Royal Pledge Proje/Assets/Scripts/PlayerScript.cs
+++ Royal Pledge Proje/Assets/Scripts/PlayerScript.cs
@@ -7,19 +7,34 @@ public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator playerAnimator;
-    
+
+    [Header("Movement")]
     // player run speed
     public float speed = 5f;
+    
+    [Space(5)]
+    public float acceleration = 7f;
+    public float deceleration = 7f;
+    
+    [Space(5)]
+    public float velPower = 0.9f;
+    
+    [Space(20)]
     
     // player jump force
     public float jumpForce = 12.2f;
     public int jumpLimit = 2;
     private int jumpCounter;
     
+    private bool facingRight = true;
+    
     // attack variables
     public int attackDamage = 50;
     public Transform attackPoint;
     public float attackRange = 0.9f;
+    private float nextAttackTime = 0f;
+    // attack per second
+    public float attackRate = 2f;
     public LayerMask enemyLayers;
     
     // player is ground check variables
@@ -32,14 +47,18 @@ public class PlayerScript : MonoBehaviour
     public Transform ceilPoint;
     private bool isCeil;
     public bool isCrouching;
-    
-    private bool facingRight = true;
 
-    private void Start()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
     }
+
+    // private void Start()
+    // {
+    //     rb = GetComponent<Rigidbody2D>();
+    //     playerAnimator = GetComponent<Animator>();
+    // }
 
     private void Update()
     {
@@ -49,7 +68,7 @@ public class PlayerScript : MonoBehaviour
         // set isCeil
         CheckCeil();
         
-        MovePlayer();
+        // MovePlayer();
         
         // player jumping
         if (Input.GetKeyDown(KeyCode.Space))
@@ -58,7 +77,7 @@ public class PlayerScript : MonoBehaviour
         }
         
         // player crouching
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.DownArrow) || isCeil)
         {
             Crouch();
         }
@@ -70,6 +89,10 @@ public class PlayerScript : MonoBehaviour
             }
             else
             {
+                if (Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    Crouch();
+                }
                 if (Input.GetKeyUp(KeyCode.DownArrow))
                 {
                     isCrouching = false;
@@ -77,14 +100,28 @@ public class PlayerScript : MonoBehaviour
             }
         }
 
-        // player do attack
-        if (Input.GetKeyDown(KeyCode.F))
+        // print("Time: " + Time.time);
+        if (Time.time >= nextAttackTime)
         {
-            Attack();
+            // player do attack
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                //print("-----------------Time: " + Time.time);
+                Attack();
+                
+                //
+                nextAttackTime = Time.time + 1f / attackRate;
+                //print("Next Attack Time: " + nextAttackTime);
+            }
         }
-        
+
         playerAnimator.SetBool("isGround", isGround);
         playerAnimator.SetBool("isCrouching", isCrouching);
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
     }
 
     private void Flip()
@@ -97,9 +134,22 @@ public class PlayerScript : MonoBehaviour
 
     private void MovePlayer()
     {
-        // player movement
+        // // player movement
+        // float horizontalInput = Input.GetAxis("Horizontal");
+        // rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+
+        # region Advanced Movement for Forces
+        
         float horizontalInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+        
+        float targetSpeed = horizontalInput * speed;
+        float speedDif = targetSpeed - rb.velocity.x;
+        float accelRate = (Math.Abs(targetSpeed) > 0.01f) ? acceleration : deceleration;
+        
+        float movement = (float)(Math.Pow(Math.Abs(speedDif) * accelRate, velPower) * Math.Sign(speedDif));
+        rb.AddForce(movement * Vector2.right);
+
+        #endregion
         
         // play run animation
         playerAnimator.SetFloat("speed", Mathf.Abs(rb.velocity.x));
@@ -175,6 +225,8 @@ public class PlayerScript : MonoBehaviour
 
     private void OnDrawGizmosSelected()
     {
+        // draw points
+        
         if (attackPoint == null || groundCheck == null || ceilPoint == null)
             return;
         
